a53d084217cabf3a156483a1353b522e0bf94969
clembrival <clemence.brival@gmail.com>
2018-04-21 22:23:44 +0100

    Changed EnemyFactory so it takes Encounter prefabs instead of enemy ones

---
+++ Assets/_Scripts/Controllers/Enemies/Encounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Encounter : MonoBehaviour
+{
+    public List<GameObject> enemyPrefabs;
+    private List<GameObject> Enemies;
+
+
+    public void CreateEnemies(GameObject target)
+    {
+        Enemies = new List<GameObject>();
+
+        for (int index = 0; index < enemyPrefabs.Count; index++)
+        {
+            GameObject enemy = Instantiate(enemyPrefabs[index],
+                enemyPrefabs[index].transform.position, Quaternion.identity);
+            enemy.GetComponent<EnemyController>().LookTarget = target;
+            Enemies.Add(enemy);
+        }
+    }
+
+    public void RemoveEnemy(GameObject enemy)
+    {
+        Enemies.Remove(enemy);
+    }
+
+    public bool IsEmpty()
+    {
+        return Enemies.Count == 0;
+    }
+}
