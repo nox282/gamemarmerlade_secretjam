a2fce1535586955ee9ae908f12cd8b88d73c5b44
nox282 <nox.tux@gmail.com>
2018-04-21 15:22:07 -0400

    added Tiles TileFactory, Camera travel and player bounds

---
+++ Assets/_Scripts/Controllers/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float TargetDistanceThreshold = 0.1f;
+    public float ScrollingSpeed = 10.0f;
+
+    private GameObject Path;
+    private Transform CP;
+    private int pathIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        Path = GameObject.FindGameObjectWithTag("Path");
+        GetNextCP();
+        transform.position = CP.transform.position;
+	}
+	
+    void GetNextCP() {
+        CP = Path.transform.GetChild(pathIndex++);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, CP.position) > TargetDistanceThreshold)
+            transform.position += (CP.position - transform.position).normalized * ScrollingSpeed * Time.deltaTime;
+        else
+            GetNextCP();
+    }
+}
---
+++ Assets/_Scripts/Entities/Tiles/Tile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public static int TileCount = 0;
+    public int Id;
+
+    public GameObject[] CPReferences;
+
+    public GameObject CheckpointPrefab;
+    public GameObject Path;
+
+    void Awake () {
+        Id = TileCount++;
+        Path = GameObject.FindGameObjectWithTag("Path");
+
+    }
+
+    void Start() {
+        MakeCheckpoints();
+    }
+
+    private void MakeCheckpoints() {
+        foreach(GameObject go in CPReferences) {
+            GenerateCheckpoint(go);
+        }
+    }
+
+    private GameObject GenerateCheckpoint(GameObject reference) {
+        return Instantiate(CheckpointPrefab,
+            reference.transform.position,
+            Quaternion.identity,
+            Path.transform);
+    }
+}
