7985b055b76632532aa3a52ebb6a119cb78ebf76
nox282 <nox.tux@gmail.com>
2018-04-22 16:41:45 -0400

    finishing up

---
+++ Assets/_Scripts/Entities/Amunitions/AmmunitionFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmunitionFactory : MonoBehaviour {
+    public List<GameObject> AmmunitionPrefabs;
+    public List<GameObject> AmmunitionList;
+    public GameObject Path;
+
+    public int PathIndex = 0;
+
+    void Start() {
+        Path = GameObject.FindGameObjectWithTag("Path");
+        AmmunitionList = new List<GameObject>();
+    }
+	
+	void Update () {
+        int oldPathIndex = PathIndex;
+        PathIndex = Camera.main.GetComponent<CameraController>().pathIndex;
+        if(PathIndex != oldPathIndex) {
+            AmmunitionList.Add(PickAmmunition());
+        }
+	}
+
+    private GameObject PickAmmunition() {
+        return Instantiate(AmmunitionPrefabs[Random.Range(0, AmmunitionPrefabs.Count)],
+            Path.transform.GetChild(PathIndex).transform.position,
+            Quaternion.identity);
+    }
+}
